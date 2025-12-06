bdcd9b975a2b6113ba82b64e01a8f1e64408061b
Dameli <dameli.kassym@nu.edu.kz>
2025-12-07 02:35:44 +0500

    Add project setup configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Dameli Kassym",
+    author_email="dameli.kassym@nu.edu.kz",
+    packages=find_packages(),
+    install_requires=[],
+)
